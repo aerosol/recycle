bb3af99d7fb85172d2f9617a3971ba419cefd784
Adam Rutkowski <hq@mtod.org>
2019-02-11 11:03:57 +0100

    Initial commit

---
+++ lib/gencycle.ex
@@ -0,0 +1,79 @@
+defmodule GenCycle do
+  @moduledoc """
+  Convienience wrapper around `:gen_cycle` behaviour for periodic, supervised 
+  task execution.
+
+  Interval is expressed in milliseconds.
+
+  Usage:
+
+    defmodule EveryMinute do
+      use GenCycle, interval: :timer.seconds(60)
+
+      def init_user_state(_opts) do
+        %{work: fn -> do_work() end}
+      end
+
+      def handle_user_cycle(%{work: fun} = state) do
+        fun.()
+        {:hibernate, state}
+      end
+    end
+  """
+
+  @callback init_user_state(list) :: any()
+  @callback handle_user_cycle(any()) ::
+              {:continue, any()}
+              | {:hibernate, any()}
+              | {:stop, any()}
+
+  defmacro __using__(use_opts) do
+    quote do
+      @behaviour :gen_cycle
+      @behaviour GenCycle
+      require Logger
+
+      def start(opts) do
+        :gen_cycle.start_supervised(
+          __MODULE__,
+          Keyword.merge(unquote(use_opts), opts)
+        )
+      end
+
+      @impl true
+      def init_cycle(opts) do
+        i = Keyword.fetch!(opts, :interval)
+
+        Logger.info("Initializing cycle: #{__MODULE__} (interval: #{i}ms)")
+
+        {:ok, {i, %{user_state: init_user_state(opts), skip_one_cycle: true}}}
+      end
+
+      @impl true
+      def handle_cycle(%{skip_one_cycle: true} = state) do
+        {:continue, %{state | skip_one_cycle: false}}
+      end
+
+      def handle_cycle(%{user_state: user_state} = state) do
+        {op, user_state} = handle_user_cycle(user_state)
+        {op, %{state | user_state: user_state}}
+      end
+
+      @impl true
+      def handle_info(:stop, _state) do
+        {:stop, :normal}
+      end
+
+      def handle_info(any, state) do
+        Logger.warn("Unexpected message #{inspect(any)}")
+        {:continue, state}
+      end
+
+      def stop(pid) when is_pid(pid) do
+        send(pid, :stop)
+      end
+
+      defoverridable handle_info: 2
+    end
+  end
+end
---
+++ test/gencycle_test.exs
@@ -0,0 +1,50 @@
+defmodule GenCycleTest do
+  use ExUnit.Case, async: true
+
+  defmodule Dummy do
+    use GenCycle, interval: 500
+
+    @impl true
+    def init_user_state(opts), do: Enum.into(opts, %{})
+
+    @impl true
+    def handle_user_cycle(%{report_to: pid, n: n} = state) do
+      send(pid, {:checking_in, n})
+      {:continue, %{state | n: n + 1}}
+    end
+  end
+
+  test "Cycles can be started and stopped" do
+    assert {:ok, cycle} = Dummy.start(a: 1)
+    ref = Process.monitor(cycle)
+
+    await(fn ->
+      assert Process.alive?(cycle)
+    end)
+
+    Dummy.stop(cycle)
+
+    assert_receive {:DOWN, ^ref, :process, _, :normal}
+  end
+
+  test "Cycles report here" do
+    assert {:ok, cycle} = Dummy.start(report_to: self(), n: 1)
+    assert_receive {:checking_in, 1}, 3_000
+    assert_receive {:checking_in, 2}, 3_000
+    Dummy.stop(cycle)
+  end
+
+  defp await(fun), do: await(1_000, fun)
+
+  defp await(0, fun), do: fun.()
+
+  defp await(timeout, fun) do
+    try do
+      fun.()
+    rescue
+      ExUnit.AssertionError ->
+        :timer.sleep(10)
+        await(max(0, timeout - 10), fun)
+    end
+  end
+end
